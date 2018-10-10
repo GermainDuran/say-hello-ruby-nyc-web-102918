d9afdc7fa9c4f5a5ebe3a924ef98f9b93c26fcb6
GERMAIN ARTURO DURAN TORRES <arturodurantorres@gmail.com>
2018-10-10 02:13:14 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,10 +1,7 @@
 # Build your say_hello method here
 def say_hello(name)
- puts "Hello Kent Beck!"
- 
+puts "Hello Kent Beck"
 end
-
-def say_hello(name ="Ruby Programmer!")
- puts ("Hello #{name}!")
- 
-end
+ def say_hello(name="Ruby Programmer!")
+  puts ("Hello #{name}!")
+end 
